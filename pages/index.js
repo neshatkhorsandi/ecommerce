b02c6d4bc28259283cbe2f438d6b048fda713e14
Neshat Khorsandi <neshat.khorsandi@edu.sait.ca>
2022-05-31 15:05:55 -0600

    finished Product component for now

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>All sorts of speakers!</p>
       </div>
+
       <div className='products-container'>
-        {['Product 1', 'Product 2'].map((product) => product)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
       <FooterBanner />
     </>
   );
 };
 
+export const getServerSideProps = async () => {
+  // fetch all data from the sanity dashboard
+  const productQuery = '*[_type == "product"]';
+  const products = await client.fetch(productQuery);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
 export default Home;
