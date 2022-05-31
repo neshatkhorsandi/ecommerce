2f58f70d4c2eedaf03c8899e31440740a8e7bb67
Neshat Khorsandi <neshat.khorsandi@edu.sait.ca>
2022-05-31 15:30:53 -0600

    implemented footer for now

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+import Link from 'next/link';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='footer-container'>
+      <p>2022 NK Headphones All rights reserved</p>
+
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+// head is the same as head in html
+// gives u metadata about ur site
+import Head from 'next/head';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }) => {
+  return (
+    <div className='layout'>
+      <Head>
+        <title>My Store</title>
+      </Head>
+
+      <header>
+        <Navbar />
+      </header>
+
+      <main className='main-container'>{children}</main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
